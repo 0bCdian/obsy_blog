d55211b5f4ee69ad6f8f695dfa05a59f3d0dc3c7
Diego Parra <diegomiguelp1996@gmail.com>
2025-01-21 11:26:45 +0100

    fix: formatting

--- src/config.ts
+++ src/config.ts
@@ -1,154 +1,154 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-	website: "https://blog.diegoparra.dev", // replace this with your deployed domain
-	author: "Diego Parra",
-	profile: "https://diegoparra.dev/",
-	desc: "Personal blog, built with Astro-Paper",
-	title: "0bsy_dev",
-	ogImage: "astropaper-og.jpg",
-	lightAndDarkMode: false,
-	showArchives: true,
-	postPerIndex: 4,
-	postPerPage: 3,
-	scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
-	editPost: {
-		url: "https://github.com/0bCdian/obsy_blog/edit/main/src/content/blog",
-		text: "Suggest Changes",
-		appendFilePath: true,
-	},
+  website: "https://blog.diegoparra.dev", // replace this with your deployed domain
+  author: "Diego Parra",
+  profile: "https://diegoparra.dev/",
+  desc: "Personal blog, built with Astro-Paper",
+  title: "0bsy_dev",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: false,
+  showArchives: true,
+  postPerIndex: 4,
+  postPerPage: 3,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  editPost: {
+    url: "https://github.com/0bCdian/obsy_blog/edit/main/src/content/blog",
+    text: "Suggest Changes",
+    appendFilePath: true,
+  },
 };
 export const LOCALE = {
-	lang: "en", // html lang code. Set this empty and default will be "en"
-	langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
 } as const;
 
 export const LOGO_IMAGE = {
-	enable: false,
-	svg: true,
-	width: 216,
-	height: 46,
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
 };
 
 export const SOCIALS: SocialObjects = [
-	{
-		name: "Github",
-		href: "https://github.com/0bCdian/",
-		linkTitle: ` ${SITE.title} on Github`,
-		active: true,
-	},
-	{
-		name: "Facebook",
-		href: "https://github.com/satnaing/astro-paper",
-		linkTitle: `${SITE.title} on Facebook`,
-		active: false,
-	},
-	{
-		name: "Instagram",
-		href: "https://github.com/satnaing/astro-paper",
-		linkTitle: `${SITE.title} on Instagram`,
-		active: false,
-	},
-	{
-		name: "LinkedIn",
-		href: "https://linkedin.com/in/diegoparranava",
-		linkTitle: `${SITE.title} on LinkedIn`,
-		active: true,
-	},
-	{
-		name: "Mail",
-		href: "mailto:diegoparranava@protonmail.com",
-		linkTitle: `Send an email to ${SITE.title}`,
-		active: true,
-	},
-	{
-		name: "BlueSky",
-		href: "https://bsky.app/profile/dmparra16.bsky.social",
-		linkTitle: `${SITE.title} on BlueSky`,
-		active: true,
-	},
-	{
-		name: "Twitch",
-		href: "https://github.com/satnaing/astro-paper",
-		linkTitle: `${SITE.title} on Twitch`,
-		active: false,
-	},
-	{
-		name: "YouTube",
-		href: "https://github.com/satnaing/astro-paper",
-		linkTitle: `${SITE.title} on YouTube`,
-		active: false,
-	},
-	{
-		name: "WhatsApp",
-		href: "https://github.com/satnaing/astro-paper",
-		linkTitle: `${SITE.title} on WhatsApp`,
-		active: false,
-	},
-	{
-		name: "Snapchat",
-		href: "https://github.com/satnaing/astro-paper",
-		linkTitle: `${SITE.title} on Snapchat`,
-		active: false,
-	},
-	{
-		name: "Pinterest",
-		href: "https://github.com/satnaing/astro-paper",
-		linkTitle: `${SITE.title} on Pinterest`,
-		active: false,
-	},
-	{
-		name: "TikTok",
-		href: "https://github.com/satnaing/astro-paper",
-		linkTitle: `${SITE.title} on TikTok`,
-		active: false,
-	},
-	{
-		name: "CodePen",
-		href: "https://github.com/satnaing/astro-paper",
-		linkTitle: `${SITE.title} on CodePen`,
-		active: false,
-	},
-	{
-		name: "Discord",
-		href: "https://github.com/satnaing/astro-paper",
-		linkTitle: `${SITE.title} on Discord`,
-		active: false,
-	},
-	{
-		name: "GitLab",
-		href: "https://github.com/satnaing/astro-paper",
-		linkTitle: `${SITE.title} on GitLab`,
-		active: false,
-	},
-	{
-		name: "Reddit",
-		href: "https://github.com/satnaing/astro-paper",
-		linkTitle: `${SITE.title} on Reddit`,
-		active: false,
-	},
-	{
-		name: "Skype",
-		href: "https://github.com/satnaing/astro-paper",
-		linkTitle: `${SITE.title} on Skype`,
-		active: false,
-	},
-	{
-		name: "Steam",
-		href: "https://github.com/satnaing/astro-paper",
-		linkTitle: `${SITE.title} on Steam`,
-		active: false,
-	},
-	{
-		name: "Telegram",
-		href: "https://github.com/satnaing/astro-paper",
-		linkTitle: `${SITE.title} on Telegram`,
-		active: false,
-	},
-	{
-		name: "Mastodon",
-		href: "https://github.com/satnaing/astro-paper",
-		linkTitle: `${SITE.title} on Mastodon`,
-		active: false,
-	},
+  {
+    name: "Github",
+    href: "https://github.com/0bCdian/",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "Facebook",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Facebook`,
+    active: false,
+  },
+  {
+    name: "Instagram",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Instagram`,
+    active: false,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://linkedin.com/in/diegoparranava",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:diegoparranava@protonmail.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: true,
+  },
+  {
+    name: "BlueSky",
+    href: "https://bsky.app/profile/dmparra16.bsky.social",
+    linkTitle: `${SITE.title} on BlueSky`,
+    active: true,
+  },
+  {
+    name: "Twitch",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Twitch`,
+    active: false,
+  },
+  {
+    name: "YouTube",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on YouTube`,
+    active: false,
+  },
+  {
+    name: "WhatsApp",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on WhatsApp`,
+    active: false,
+  },
+  {
+    name: "Snapchat",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Snapchat`,
+    active: false,
+  },
+  {
+    name: "Pinterest",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Pinterest`,
+    active: false,
+  },
+  {
+    name: "TikTok",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on TikTok`,
+    active: false,
+  },
+  {
+    name: "CodePen",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on CodePen`,
+    active: false,
+  },
+  {
+    name: "Discord",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Discord`,
+    active: false,
+  },
+  {
+    name: "GitLab",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on GitLab`,
+    active: false,
+  },
+  {
+    name: "Reddit",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Reddit`,
+    active: false,
+  },
+  {
+    name: "Skype",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Skype`,
+    active: false,
+  },
+  {
+    name: "Steam",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Steam`,
+    active: false,
+  },
+  {
+    name: "Telegram",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Telegram`,
+    active: false,
+  },
+  {
+    name: "Mastodon",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Mastodon`,
+    active: false,
+  },
 ];
